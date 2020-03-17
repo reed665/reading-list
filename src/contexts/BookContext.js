f0b0178adbeea2b1d90ff20d7e452e15e67fa1a3
reed665 <reed665@hotmail.com>
2020-03-17 11:52:00 +0300

    BookContext

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,44 @@
+import React, { createContext, useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import valarg from 'valarg';
+
+export const BookContext = createContext();
+
+const newBook = (title, author) => {
+  valarg(title, { type: String, required: true })
+  valarg(author, { type: String })
+
+  return {
+    id: uuid(),
+    title,
+    author,
+  }
+}
+
+const BookContextProvider = (props) => {
+  const [books, setBooks] = useState([
+    newBook('name of the wind', 'patrick rothuss'),
+    newBook('the final empire', 'brandon sanderson'),
+  ])
+
+  const addBook = (title, author) => {
+    setBooks([
+      ...books,
+      newBook(title, author),
+    ])
+  }
+
+  const removeBook = (id) => {
+    setBooks(
+      books.filter(book => book.id !== id)
+    )
+  }
+
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  )
+}
+
+export default BookContextProvider;
