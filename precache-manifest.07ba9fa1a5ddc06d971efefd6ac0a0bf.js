658ccf568954434f92c26520bbefeb7fd4f699b4
reed665 <reed665@hotmail.com>
2020-03-20 12:38:57 +0300

    Updates

---
+++ precache-manifest.07ba9fa1a5ddc06d971efefd6ac0a0bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbba9bac87cd1171d9ef6748c52d6d1f",
+    "url": "/reading-list/index.html"
+  },
+  {
+    "revision": "49ac1eded058be8402da",
+    "url": "/reading-list/static/css/main.7fd6f670.chunk.css"
+  },
+  {
+    "revision": "444be1a326dfc62dbbbe",
+    "url": "/reading-list/static/js/2.f9994b0b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reading-list/static/js/2.f9994b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ac1eded058be8402da",
+    "url": "/reading-list/static/js/main.38df600d.chunk.js"
+  },
+  {
+    "revision": "399f06f921fae714ffe2",
+    "url": "/reading-list/static/js/runtime-main.1bb88c69.js"
+  }
+]);
